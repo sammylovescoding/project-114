327143d1923582c8d641c7d6c69f46081ff9fed5
sammylovescoding <92454492+sammylovescoding@users.noreply.github.com>
2022-11-08 16:54:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function prelod(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+     save('MyFilterImage.png');
+}
